c7d601ae6e5c8e6b7140fb531db71678e488fde4
jerahmeel princewill <jamicojerahmeel@gmail.com>
2024-12-19 17:00:54 +0100

    feat/famous quotes question 5

--- quotes.js
+++
@@ -1,3 +0,0 @@
-console.log("Albert Einstein once said, “A person who never made a mistake never tried anything new.”")
-console.log("Franklin D. Roosevelt, “The only limit to our realization of tomorrow is our doubts of today.” ")
-console.log("Thomas Edison, “I have not failed. I've just found 10,000 ways that won't work.”")
