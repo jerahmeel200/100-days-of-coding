a8bb2cec18e268916995f3bdaa290f94ba103752
jerahmeel princewill <jamicojerahmeel@gmail.com>
2024-12-19 16:47:50 +0100

    feat/day-one concatinating personal message

---
+++ Day-one-Personal-Message/message.js
@@ -0,0 +1,8 @@
+const firstName = "David"
+const secondName = "john"
+
+// using + sign to concatinate
+console.log("Hello" + " " +  firstName  + " " + "would you like learn javascript today?")
+
+// using object litral
+console.log(`Hello ${secondName} would you like to lern python`)
