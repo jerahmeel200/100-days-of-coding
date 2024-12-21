56f14f6e5d354ebfe191558a5256b8ca04f5a9af
jerahmeel princewill <jamicojerahmeel@gmail.com>
2024-12-21 23:55:14 +0100

    feat: day-6 question 18

---
+++ Day-6/q18/main.js
@@ -0,0 +1,13 @@
+ var places = ["paris", "cuba", "lagos", "abuja", "spain"]
+
+ console.log(places) 
+
+//  sort place alphabetically using the sort method
+
+const sortedPlaces = places.sort()
+console.log(sortedPlaces)
+
+// printed in reverse-alphabetical order using the localeCompare Methosd
+
+const reverseAphabetical = sortedPlaces.reverse()
+console.log(reverseAphabetical)
