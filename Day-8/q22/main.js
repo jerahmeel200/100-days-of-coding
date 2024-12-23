82f1097ce148c3e683c77d80227ef14c9eed54ad
jerahmeel princewill <jamicojerahmeel@gmail.com>
2024-12-23 21:00:37 +0100

    feat: Day-8 question 22

---
+++ Day-8/q22/main.js
@@ -0,0 +1,7 @@
+ 
+
+ const friends  = ["John", "Jane", "Doe", "Harry", "Larry", "Moe"];
+
+//  console.log(friends[6]) //an intentional index error
+
+console.log(friends[5]) //error corrected valid index accessed
---
+++ Day-8/q22/main.ts
@@ -0,0 +1,18 @@
+ (()=>{
+      
+
+ const friends : string[]  = ["John", "Jane", "Doe", "Harry", "Larry", "Moe"];
+
+ //  console.log(friends[6]) //an intentional index error
+ 
+ console.log(friends[5]) //error corrected valid index accessed
+ 
+   
+   })()
+
+
+
+
+   
+ 
+ 
