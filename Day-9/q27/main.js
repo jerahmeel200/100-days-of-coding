ef36a00a28fbb2aaa03c66fd0435f7e551735d72
jerahmeel princewill <jamicojerahmeel@gmail.com>
2024-12-24 22:28:42 +0100

    feat: Day-9 question 27

--- Day-9/q27/main.js
+++ Day-9/q27/main.js
@@ -1,19 +1,41 @@
-(function () {
+ 
 // the version that runs the if block
-  var alienColor = "green"
-
-  if(alienColor === "green"){
-    console.log("You have just earned 5 points for shooting the alien.")
-  }else{
-    console.log("You have earned 10 points")
-  }
+  alienColor = "purple"
 
   
-// the version that runs the else block
-  alienColor = "red"
+// green version
+  alienColor = "green"
   if(alienColor === "green"){
-    console.log("You have earned 5 points for shooting the alien.")
-  }else{
+    console.log("You have just earned 5 point")
+  }else if(alienColor === "yellow"){
+    console.log("You have earned 10 points")
+  }else if (alienColor === "red"){
+    console.log("You have earned 15 points")
+  }
+  
+
+
+  // yelllow version
+  alienColor = "yellow"
+  if(alienColor == "yellow"){
+    console.log("You have earned 10 points")
+  }else if(alienColor == "green"){
+    console.log("You have just earned 5 ")
+  }else if (alienColor == "red"){
+    console.log("You have earned 15 points")
+  }
+
+
+  // red version
+  alienColor = "red"
+  if(alienColor === "red"){
+    console.log("You have earned 15 points")
+  }else if(alienColor === "green"){
+    console.log("You have just earned 5 points")
+  }else if (alienColor === "yellow"){
     console.log("You have earned 10 points")
   }
-})();
+  
+ 
+ 
+  
--- Day-9/q27/main.ts
+++ Day-9/q27/main.ts
@@ -1,15 +1,44 @@
  (()=>{
-   let AlienColor :string = "red";
+  
+// the version that runs the if block
+var alienColor = "purple"
 
-   if(AlienColor === "red"){
-       console.log("you have earned 5 points!!")
-   }
-   
-   
-   AlienColor = "green";
-   if(AlienColor == "red"){
-   //    no output because the condition is false
-   }
+  
+// green version
+  alienColor = "green"
+  if(alienColor === "green"){
+    console.log("You have just earned 5 point")
+  }else if(alienColor === "yellow"){
+    console.log("You have earned 10 points")
+  }else if (alienColor === "red"){
+    console.log("You have earned 15 points")
+  }
+  
+
+
+  // yelllow version
+  alienColor = "yellow"
+  if(alienColor == "yellow"){
+    console.log("You have earned 10 points")
+  }else if(alienColor == "green"){
+    console.log("You have just earned 5 ")
+  }else if (alienColor == "red"){
+    console.log("You have earned 15 points")
+  }
+
+
+  // red version
+  alienColor = "red"
+  if(alienColor === "red"){
+    console.log("You have earned 15 points")
+  }else if(alienColor === "green"){
+    console.log("You have just earned 5 points")
+  }else if (alienColor === "yellow"){
+    console.log("You have earned 10 points")
+  }
+  
+ 
+ 
    
    })()
 
