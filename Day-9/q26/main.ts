e1757a6d343a79db2f021ee796830c0647c4b1b1
jerahmeel princewill <jamicojerahmeel@gmail.com>
2024-12-24 22:06:54 +0100

    feat: Day-9 question 26

---
+++ Day-9/q26/main.ts
@@ -0,0 +1,21 @@
+ (()=>{
+   let AlienColor :string = "red";
+
+   if(AlienColor === "red"){
+       console.log("you have earned 5 points!!")
+   }
+   
+   
+   AlienColor = "green";
+   if(AlienColor == "red"){
+   //    no output because the condition is false
+   }
+   
+   })()
+
+
+
+
+   
+ 
+ 
