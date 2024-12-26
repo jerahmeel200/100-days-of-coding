bd2ffb2cd77335d60d3633d0f5a37125a2254e2d
jerahmeel princewill <jamicojerahmeel@gmail.com>
2024-12-26 15:11:55 +0100

    feat: Day-11  question 30

---
+++ Day-11/q31/main.js
@@ -0,0 +1,8 @@
+let userName = [];
+ 
+    if (userName.length  === 0){
+        console.log("we need to find some users!")
+    }else{
+    //
+    }
+ 
---
+++ Day-11/q31/main.ts
@@ -0,0 +1,13 @@
+ (()=>{
+
+    const userName : string[] = ["admin", "eric", "terry"];
+
+    userName.forEach((name)=>{
+       if(name  == "admin"){
+           console.log("Hello admin, would you like to see a status report?");
+       }else{
+           console.log(`Hello  ${name} , thank you for logging`)
+       }
+    })
+
+ })()
