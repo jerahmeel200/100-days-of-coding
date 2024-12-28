606bfbbde3d1dd5f8dfa3abc4976d8edc75cf0e0
jerahmeel princewill <jamicojerahmeel@gmail.com>
2024-12-28 07:01:33 +0100

    feat: Day-12 question 36

---
+++ Day-13/q37/main.js
@@ -0,0 +1,11 @@
+ 
+const make_shirt = (size="large", text="i love javaScript") =>{
+ 
+  console.log(`The size of the shirt is ${size} and the text on the shirt is ${text}`)
+}
+make_shirt()
+make_shirt("medium")
+make_shirt("small", "I love Python")
+ 
+
+ 
