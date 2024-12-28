aa2747b0a15b586528bcfbdbf3dbe69c8d8c2876
jerahmeel princewill <jamicojerahmeel@gmail.com>
2024-12-28 14:54:07 +0100

    feat: Day-13 question 38

---
+++ Day-13/q38/main.js
@@ -0,0 +1,14 @@
+ 
+const describe_city = (city, country="india") =>{
+ 
+  console.log(`the city of ${city} is in ${country}`)
+}
+describe_city("agra")
+describe_city("chennai")
+describe_city("islamabad", "pakistan")
+ 
+
+ 
+
+
+ 
---
+++ Day-13/q38/main.ts
@@ -0,0 +1,14 @@
+ (()=>{
+ 
+  const describe_city = (city: string, country: string ="india") =>{
+ 
+    console.log(`the city of ${city} is in ${country}`)
+  }
+  describe_city("agra")
+  describe_city("chennai")
+  describe_city("islamabad", "pakistan")
+   
+  
+   
+
+ })()
