d183f361c7128233733f99ea0e6067e561ad1733
jerahmeel princewill <jamicojerahmeel@gmail.com>
2024-12-28 15:55:19 +0100

    feat: Day-13 question 39

---
+++ Day-13/q39/main.js
@@ -0,0 +1,14 @@
+ 
+const describe_city = (city, country) =>{
+ 
+  console.log(`${city}, ${country}`)
+}
+describe_city("jos", "nigeria")
+describe_city("chennai", "india")
+describe_city("islamabad", "pakistan")
+ 
+
+ 
+
+
+ 
---
+++ Day-13/q39/main.ts
@@ -0,0 +1,17 @@
+ (()=>{
+ 
+  const describe_city = (city, country )  =>{
+ 
+   return  `${city}, ${country}`
+  }
+  console.log(describe_city("jos", "nigeria"))
+  console.log(describe_city("chennai", "india"))
+  console.log(describe_city("islamabad", "pakistan"))
+  
+   
+  
+   
+  
+   
+
+ })()
