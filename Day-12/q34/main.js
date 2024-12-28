0617fcfba8601932ec9de0ce2a0139776c723036
jerahmeel princewill <jamicojerahmeel@gmail.com>
2024-12-28 05:55:10 +0100

    feat: Day-12 question 34

---
+++ Day-12/q34/main.js
@@ -0,0 +1,10 @@
+ 
+    var pizza = ["pepperoni", "sicillian", "greek", "Grandma"];
+    // i terate through the array
+    for (var i = 0; i < pizza.length; i++) {
+        // print the current pizza and the message
+        console.log("i like ".concat(pizza[i], " pizza"));
+    }
+    // print the message
+    console.log("I really love pizza");
+ 
---
+++ Day-12/q34/main.ts
@@ -0,0 +1,10 @@
+ (()=>{
+    const pizza: string[] = ["pepperoni", "sicillian", "greek", "Grandma"];
+
+    for(let i = 0 ; i < pizza.length; i++){
+       console.log( `i like ${pizza[i]} pizza`)
+    }
+    
+    console.log("I really love pizza")
+
+ })()
