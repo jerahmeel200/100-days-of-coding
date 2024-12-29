59000b1aad5f6d8c707ee64bea42c502b3e9b020
jerahmeel princewill <jamicojerahmeel@gmail.com>
2024-12-29 23:30:56 +0100

    feat: Day-14 question 41

---
+++ Day-14/q41/main.js
@@ -0,0 +1,14 @@
+let magicians = ["Gandalf", "Dumbledore", "Merlin", "Houdini", "Potter"];
+
+const show_magicians =(magicians)=>{
+  magicians.forEach((magician)=>{
+      console.log(magician)
+    })
+}
+
+ show_magicians(magicians)
+
+ 
+
+
+ 
---
+++ Day-14/q41/main.ts
@@ -0,0 +1,14 @@
+ (()=>{
+ 
+  let  magicians: string[] = ["Gandalf", "Dumbledore", "Merlin", "Houdini", "Potter"]
+
+
+  const show_magicians = (magicians: string[])=>{
+    magicians.forEach((magician)=>{
+console.log(magician)
+    })
+  }
+  
+  show_magicians(magicians)
+
+ })()
