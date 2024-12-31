cff7800272663d5c431fd482b2cc9c631ee14a18
jerahmeel princewill <jamicojerahmeel@gmail.com>
2024-12-31 14:14:40 +0100

    feat: Day-16  question 47

--- Day-16/q47/main.js
+++ Day-16/q47/main.js
@@ -3,17 +3,12 @@
 const laptops = [
   { make: "Apple", model: "MacBook Pro", year: 2021 },
   { make: "Dell", model: "XPS 13", year: 2020 },
-  { make: "HP", model: "Spectre x360", year: 2022 }
+  { make: "HP", model: "Spectre x360", year: 2022 },
 ];
 
 // Using array destructuring to assign the first and second laptops
-const [firstLaptop, secondLaptop] = laptops
-
-
+const [firstLaptop, secondLaptop] = laptops;
 
 // Logging the variables
-console.log("first laptop",firstLaptop)
-console.log("second laptop", secondLaptop)
-
-
- 
+console.log("first laptop", firstLaptop);
+console.log("second laptop", secondLaptop);
