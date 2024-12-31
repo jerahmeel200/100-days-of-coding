febb8d89da2247a7c4abc8dfe5c379752dc31777
jerahmeel princewill <jamicojerahmeel@gmail.com>
2024-12-31 14:10:03 +0100

    feat: Day-16  question 47

--- Day-16/q46/main.js
+++ Day-16/q46/main.js
@@ -8,12 +8,7 @@
  }
 laptop.describe()
 
-// OR  YOU CAN  USE THE MAP METHOD
-// const describe = ()=>{
-//   laptop.map((item)=>{
-//     console.log(`the laptops is made by ${item.make} and the model is ${item.model} produced in the year ${item.year}`)
-//   })
-// }
+ 
 
 
-// describe()
+ 
---
+++ Day-16/q47/main.js
@@ -0,0 +1,19 @@
+// Array of objects representing laptops
+
+const laptops = [
+  { make: "Apple", model: "MacBook Pro", year: 2021 },
+  { make: "Dell", model: "XPS 13", year: 2020 },
+  { make: "HP", model: "Spectre x360", year: 2022 }
+];
+
+// Using array destructuring to assign the first and second laptops
+const [firstLaptop, secondLaptop] = laptops
+
+
+
+// Logging the variables
+console.log("first laptop",firstLaptop)
+console.log("second laptop", secondLaptop)
+
+
+ 
