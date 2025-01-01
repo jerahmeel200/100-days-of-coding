c37e6ae7c6975fec55b72db0b617407e8b7b95c7
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-01-01 12:22:39 +0100

    feat: Day-17 Question 50

---
+++ Day-17/q50/main.js
@@ -0,0 +1,4 @@
+let myIdealDay = "My ideal day would involve 1. Waking up early and going for a jog.2. Spending a few hours coding on a personal project. 3. Ending the day by reading a good book.";
+
+// Logging the multiline string to the console
+console.log(myIdealDay);
