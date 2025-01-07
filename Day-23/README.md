ffe710c418f087cefddf5e10ab463f4fefb9d4d9
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-01-07 23:06:44 +0100

    feat: Day-23 uestion 77

---
+++ Day-23/README.md
@@ -0,0 +1,10 @@
+# Day-23 of `100-Days-Of-Code` Challenge
+
+**Q67** - Arithmetic with Mixed Types: Write a function that takes two parameters: a number and a string that represents a number (e.g., "5"). Return their sum as a number.
+
+ 
+ 
+ 
+
+
+ 
---
+++ Day-23/q67/main.js
@@ -0,0 +1,11 @@
+ 
+function addNumberAndString(number1 , numberString )  {
+  // Converts the string to a number and adds it to the first number
+  return number1 + Number(numberString);
+}
+
+ 
+console.log(addNumberAndString(10, "5")); // Shows 15
+// The string "5" is turned into the number 5, and then added to 10.
+
+ 
