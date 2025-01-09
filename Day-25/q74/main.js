c8f44dfed7adb55b2824b74ae4404c594aa16f18
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-01-09 21:01:16 +0100

    feat: Day-24 question 74

---
+++ Day-25/q74/main.js
@@ -0,0 +1,23 @@
+ 
+
+
+
+function swap (){
+  let a = 5
+      b =10
+
+      console.log("Before swap: a = ", a, "and b = ", b)
+
+      let temp = a
+      a = b
+       b = temp
+
+       console.log("After swap: a = ", a, "and b = ", b)
+
+  
+}
+
+
+swap()
+
+ 
---
+++ Day-25/q74/main.ts
@@ -0,0 +1,44 @@
+ 
+(()=>{
+ 
+
+
+
+  function swap (){
+    let a = 5
+    let b = 10
+  
+        console.log("Before swap: a = ", a, "and b = ", b)
+  
+        let temp = a
+        a = b
+         b = temp
+  
+         console.log("After swap: a = ", a, "and b = ", b)
+  
+    
+  }
+  
+  
+  swap()
+  
+   
+        
+  
+  
+   
+
+})()
+
+ 
+
+
+
+
+
+
+
+
+
+
+
