7eed61ad40c9860963091e055de8b5ed75547b6b
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-01-11 22:34:41 +0100

    feat: Day-27  question 80

--- Day-27/README.md
+++ Day-27/README.md
@@ -2,6 +2,8 @@
 
 **Q79** - Creating and Accessing Object Properties: Construct an object representing a car with properties for make, model, and year. Then, show how you can access the model property of the car.
  
+
+ Updating Object Properties: Add a property named `color` to the existing car object, and then update the `year` property to `2021`. Show how to perform these operations.
  
 
 
---
+++ Day-27/q80/main.ts
@@ -0,0 +1,39 @@
+ 
+(()=>{
+ 
+  const car =
+  {
+       make:"Honda",
+       model:"civic RTX",
+       year: "2013"
+  }
+
+
+
+// one way to access the object is by using the dot (.) notation
+// if you are working with array of object, to  access the model, make,
+//  and year properties, you need to reference the first element
+//  in the array using an index, like this: console.log(car[0].model)
+console.log(car.model)
+console.log(car.make)
+console.log(car.model)
+
+
+   
+  
+   
+
+})()
+
+ 
+
+
+
+
+
+
+
+
+
+
+
