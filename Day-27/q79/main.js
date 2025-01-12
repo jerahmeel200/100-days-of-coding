9c2ff04f1cbb0a3dd5efa7ad05d1f5d9e637b065
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-01-12 02:33:00 +0100

    feat: Day-27  question 80

--- Day-27/q79/main.js
+++ Day-27/q79/main.js
@@ -8,9 +8,6 @@ const car =
 
 
 // one way to access the object is by using the dot (.) notation
-// if you are working with array of object, to  access the model, make,
-//  and year properties, you need to reference the first element
-//  in the array using an index, like this: console.log(car[0].model)
 console.log(car)
  
 
