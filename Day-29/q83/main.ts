470fa95603b90dff8839e3e535e953c7d43d08b9
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-01-13 02:20:27 +0100

    feat: Day-29  question 85

---
+++ Day-29/q83/main.ts
@@ -0,0 +1,26 @@
+ 
+(()=>{
+ 
+   
+function findCodePosition(str: string){
+   return str.indexOf("code")  
+  }
+  
+  console.log("code is at position",
+      findCodePosition("i love to code in javascript")
+  )
+
+})()
+
+ 
+
+
+
+
+
+
+
+
+
+
+
