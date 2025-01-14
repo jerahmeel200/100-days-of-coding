63bfb9a5c40616659709066e416c747270b905ee
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-01-14 21:28:17 +0100

    feat: Day-30  question 89

---
+++ Day-30/q89/main.js
@@ -0,0 +1,9 @@
+ 
+function RoundingNumbers(num){
+    // we can do like so
+// return    Math.round(num * 100)/ 100
+return Number(num.toFixed(2))
+}
+
+console.log(RoundingNumbers(1.2534456))
+ 
---
+++ Day-30/q89/main.ts
@@ -0,0 +1,27 @@
+ 
+(()=>{
+ 
+   
+ 
+  function RoundingNumbers(num){
+    // we can do like so
+// return    Math.round(num * 100)/ 100
+return Number(num.toFixed(2))
+}
+
+console.log(RoundingNumbers(1.2534456))
+
+})()
+
+ 
+
+
+
+
+
+
+
+
+
+
+
