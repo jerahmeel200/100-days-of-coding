2c2f3b7f6cd125b3750860ca169cea7a734edfff
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-01-18 23:28:27 +0100

    feat: Day-34  question 101

---
+++ Day-34/q101/main.js
@@ -0,0 +1,5 @@
+ const randomNumber = ()=>{
+return Math.floor(Math.random() * 10) +1
+ }
+
+console.log(randomNumber())
---
+++ Day-34/q101/main.ts
@@ -0,0 +1,20 @@
+ 
+(()=>{
+ 
+  const squareRoot: number  = Math.sqrt(144)
+
+  console.log(`The square root of  144 is ${squareRoot}`)
+})()
+
+ 
+
+
+2
+
+
+
+
+
+
+
+
