a58a5d00a287dfa18ff49c81bcd3f71c19eeea55
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-01-20 14:05:50 +0100

    feat: Day-36  question 107

---
+++ Day-36/q107/main.ts
@@ -0,0 +1,25 @@
+(() => {
+    const divisible = (num: number ) => {
+        if (num % 2 === 0 && num % 3 === 0) {
+          return `${num} is divisible by 2 and 3`;
+        } else {
+          return `${num} is not divisible by 2 and 3`;
+        }
+      };
+      
+      console.log(divisible(12));  
+      
+      
+      
+      // Another way without the function
+      
+      const number: number = 9
+      if(number % 2 === 0 && number % 3 === 0){
+        console.log(`${number} is  divisible by 2 and 3`)
+      }else{
+        console.log(`${number} is not divisible by 2 and 3`)
+      
+      }
+      
+})();
+ 
