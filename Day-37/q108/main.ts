f24c1314f026b7ae4ef9c5246bb3a822f701bef9
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-01-21 20:46:20 +0100

    feat: Day-37  question 109

--- Day-37/q108/main.ts
+++
@@ -1,9 +0,0 @@
-(() => {
-  const compareString = (str1: string, str2: string)=>{
-    return  str1.toLowerCase() === str2.toLowerCase()
-    }
-   
-    console.log(compareString("HELLO", "hello"))
-      
-})();
- 
