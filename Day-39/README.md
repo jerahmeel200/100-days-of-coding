f9975752217694e994918960e2b5bfa771bd0a4b
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-01-23 17:24:47 +0100

    feat: Day-39  question 116

--- Day-39/README.md
+++ Day-39/README.md
@@ -1,8 +1,8 @@
 # Day-39 of `100-Days-Of-Code` Challenge
 
-**Q114** - Use a switch statement to log the days of the week based on a number (1-7).
+**Q115** - Use a switch statement to log the days of the week based on a number (1-7).
 
- 
+**Q116** - Create a switch case that matches several cases to the same code block, representing seasons.
 
  
 
---
+++ Day-39/q116/main.ts
@@ -0,0 +1,37 @@
+(() => {
+  const seasons = (month: number): void => {
+    switch (month) {
+      case 1:
+      case 2:
+      case 3:
+        console.log("winter");
+        break;
+      case 4:
+      case 5:
+      case 6:
+        console.log("spring");
+        break;
+      case 7:
+      case 8:
+      case 9:
+        console.log("summer");
+        break;
+      case 10:
+      case 11:
+      case 12:
+        console.log("fall");
+        break;
+      default:
+        console.log("invalid");
+        break;
+    }
+  };
+  
+  seasons(5);
+  
+      
+
+
+ 
+})();
+ 
