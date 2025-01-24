69357e5d14783b1d3fdab7c44f3381b03d016981
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-01-24 20:16:19 +0100

    feat: Day-40  question 119

--- Day-40/q117/main.ts
+++ Day-40/q117/main.ts
@@ -1,29 +1,5 @@
 (() => {
-  const evaluateGrade = (grade: string): void => {
-    switch (grade) {
-      case "A" :
-      console.log("Excellent")
-      break
-      case "B" :
-      console.log("Good")
-      break
-      case "C" :
-      console.log("Fair")
-      break
-      case "E" :
-      console.log("Poor")
-      break
-      case "F" :
-      console.log("Fail")
-      break
-      default:
-        console.log("invalid grade");
-        break;
-    }
-  };
-  
-  evaluateGrade("B");
-  
+ 
       
 
 
