413de89308606cc018679f899d5e382795aafb9c
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-01-30 14:21:39 +0100

    feat: Day-45  question 135

--- Day-45/q135/main.js
+++ Day-45/q135/main.js
@@ -1,7 +1,21 @@
-const json = '{   "name" : "alice", "age": 35}'
-
-
-const obj = JSON.parse(json)
-
-
-console.log(obj)
+ 
+const person = {
+    name: "Alice",
+    age: 30,
+    city: "Wonderland",
+  };
+  
+  // Converts the object into a JSON string with indentation
+  const jsonString = JSON.stringify(person, null, 2); 
+  // The '2' specifies the number of spaces to use as white space
+  
+  console.log(jsonString);
+  /* Outputs:
+  {
+    "name": "Alice",
+    "age": 30,
+    "city": "Wonderland"
+  }
+  */
+  // Shows how adding indentation to the JSON string 
+  // makes it easier to read.
