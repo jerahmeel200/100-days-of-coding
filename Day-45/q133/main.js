f283621628b062359ddd5bfec2fae87b6bb44ce8
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-01-30 13:49:33 +0100

    feat: Day-45  question 133

---
+++ Day-45/q133/main.js
@@ -0,0 +1,8 @@
+ const person = {
+  name: "Alice",
+  age: 35,
+  role: "Developer"
+ }
+
+ const jsonString = JSON.stringify(person)
+ console.log(jsonString)
---
+++ Day-45/q133/main.ts
@@ -0,0 +1,15 @@
+ 
+ 
+(() => {
+
+const person = {
+  name: "Alice",
+  age: 35,
+  role: "Developer"
+ }
+
+ const jsonString = JSON.stringify(person)
+ console.log(jsonString)
+             
+  
+})();
