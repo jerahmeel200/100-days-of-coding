abc4f0a1d89b5df4837ae53718dbb429e9ba98c3
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-02-01 22:44:24 +0100

    feat: Day-48  question 144

--- Day-48/q144/main.ts
+++ Day-48/q144/main.ts
@@ -1,19 +1,30 @@
  
  
 (() => {
+const pizza = new Promise<string>((resolve, reject)=>{
+    setTimeout(() => resolve("Pizza Delvered"), 2000)
+})
+const burger = new Promise<string>((resolve, reject)=>{
+    setTimeout(() => resolve("Pizza Delvered"), 1500)
+})
+const drinks = new Promise<string>((resolve, reject)=>{
+    setTimeout(() => resolve("Pizza Delvered"), 1000)
+})
 
- const myPromise = new Promise<string> ((resolve, reject) =>{
-const success = Math.random() > 0.5 
+// if one of the orders reject of fail the catch will trigger
 
-if(success){
-    resolve("Promise resolved successfully")
-}else{
-    reject("Failure")
-}
+// Promise.all([returns all the Promise in a array if one of the promise fail
+//  the carch will trigger and it will not  return any of the resolved promise ])
+Promise.all([pizza, burger, drinks])
 
-myPromise
-    .then((message)=> console.log(message))
-    .catch((message)=> console.log(message))
- })
+.then((message)=> console.log(message))
+.catch((error)=>{
+    console.log("One of the orders failed", error)
+})
+
+//  Promise.all is used to wait for all three promises to resolve, and then it logs the array of their results, [],
+//  to the console. This shows how Promise.all can handle multiple promises simultaneously.
+
+ 
 })();
 
