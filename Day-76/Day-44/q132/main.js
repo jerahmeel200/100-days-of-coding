2284020b20170a4ca91800c2ecce0fd85c814df5
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-03-01 22:48:13 +0100

    feat: Day-76

---
+++ Day-76/Day-44/q132/main.js
@@ -0,0 +1,24 @@
+// Importing named exports:
+import { utilOne, utilTwo } from "./utils";
+
+// Importing a default export:
+import Calculator from "./Calculator";
+
+// This code illustrates the syntax and usage differences 
+// between default and named exports.
+              
+ 
+
+export default class Calculator {
+  // Class definition
+}
+
+ 
+
+// Named export example in file: utils.ts
+export const utilOne = () => {
+  /*...*/
+};
+export const utilTwo = () => {
+  /*...*/
+};     
