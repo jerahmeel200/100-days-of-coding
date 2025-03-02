5c2aad3066bdc24ce65f9fe79b70151ea869bb1d
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-03-02 22:18:07 +0100

    feat: Day-59 code update

---
+++ Day-59/main.js
@@ -0,0 +1,101 @@
+//  Loops: while and for
+
+// What is the last value alerted by this code? Why?
+
+
+let i = 3;
+
+while (i) {
+  alert( i-- );
+}
+// the Output will be 1 because the condition is true and the loop will run until
+//  the condition is false. in this case when the loop reaches zero the loop will stop or break.
+
+
+
+
+
+// For every loop iteration, write down which value it outputs and then compare it with the solution.
+// The task demonstrates how postfix/prefix forms can lead to different results when used in comparisons.
+
+// From 1 to 4
+
+// let i = 0;
+while (++i < 5) alert( i );
+// The first value is i = 1, because ++i first increments i and then returns the new value. So the first comparison is 1 < 5 and the alert shows 1.
+
+// Then follow 2, 3, 4… – the values show up one after another. The comparison always uses the incremented value, because ++ is before the variable.
+
+// Finally, i = 4 is incremented to 5, the comparison while(5 < 5) fails, and the loop stops. So 5 is not shown.
+
+// From 1 to 5
+
+// let i = 0;
+while (i++ < 5) alert( i );
+// The first value is again i = 1. The postfix form of i++ increments i and then returns the old value, so the comparison i++ < 5 will use i = 0 (contrary to ++i < 5).
+
+// But the alert call is separate. It’s another statement which executes after the increment and the comparison. So it gets the current i = 1.
+
+// Then follow 2, 3, 4…
+
+// Let’s stop on i = 4. The prefix form ++i would increment it and use 5 in the comparison. But here we have the postfix form i++. So it increments i to 5, but returns the old value. Hence the comparison is actually while(4 < 5) – true, and the control goes on to alert.
+
+// The value i = 5 is the last one, because on the next step while(5 < 5) is false.
+
+
+
+
+// Rewrite the code changing the for loop to while without altering its behavior (the output should stay same).
+
+for (let i = 0; i < 3; i++) {
+  alert( `number ${i}!` );
+}
+
+
+// let i  = 0
+while(i < 3){
+    alert(`number ${i}!`)
+    i++
+}
+
+
+// Write a loop which prompts for a number greater than 100. If the visitor enters another number – ask them to input again.
+
+// The loop must ask for a number until either the visitor enters a number greater than 100 or cancels the input/enters an empty line.
+
+// Here we can assume that the visitor only inputs numbers. There’s no need to implement a special handling for a non-numeric input in this task.
+
+let number
+
+let num;
+
+do {
+  num = prompt("Enter a number greater than 100?", 0);
+} while (num <= 100 && num);
+
+
+
+// An integer number greater than 1 is called a prime if it cannot be divided without a remainder by anything except 1 and itself.
+
+// In other words, n > 1 is a prime if it can’t be evenly divided by anything except 1 and n.
+
+// For example, 5 is a prime, because it cannot be divided without a remainder by 2, 3 and 4.
+
+// Write the code which outputs prime numbers in the interval from 2 to n.
+
+// For n = 10 the result will be 2,3,5,7.
+
+// P.S. The code should work for any n, not be hard-tuned for any fixed value
+
+
+let n = 10;
+
+nextPrime:
+for (let i = 2; i <= n; i++) { // for each i...
+
+  for (let j = 2; j < i; j++) { // look for a divisor..
+    if (i % j == 0) continue nextPrime; // not a prime, go next i
+  }
+
+  alert( i ); // a prime
+}
