9531aafe75d1c3ef83704aa88905cfe8100ac153
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-03-11 22:15:33 +0100

    feat: Day-85  task

---
+++ Day-85/Day-44/q131/main.js
@@ -0,0 +1,6 @@
+ import Person from "./person";
+
+
+const newPerson = new Person("Alice", 34)
+
+console.log(newPerson.greet())            
---
+++ Day-85/Day-44/q131/main.ts
@@ -0,0 +1,9 @@
+import Person from "./person";
+(() => {
+
+
+  const newPerson = new Person("Alice", 34)
+  
+  console.log(newPerson.greet())          
+  
+})();
